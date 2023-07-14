50d0c84cda0e8a4c5940c5e7364d391bb15a71bd
PythJavHacker <adam.eldesoky@gmail.com>
2023-07-15 01:25:08 +0300

    Added the decoder

--- Generator.py
+++ Generator.py
@@ -27,6 +27,35 @@ Encode = {'a': 'c',
           ' ': ' '}
 
 
+Decode = {'c': 'a',
+          'd': 'b',
+          'e': 'c',
+          'f': 'd',
+          'g': 'e',
+          'h': 'f',
+          'i': 'g',
+          'j': 'h',
+          'k': 'i',
+          'l': 'j',
+          'm': 'k',
+          'n': 'l',
+          'o': 'm',
+          'p': 'n',
+          'q': 'o',
+          'r': 'p',
+          's': 'q',
+          't': 'r',
+          'u': 's',
+          'v': 't',
+          'w': 'u',
+          'x': 'v',
+          'y': 'w',
+          'z': 'x',
+          'a': 'y',
+          'b': 'z',
+          ' ': ' '}
+
+
 def encode():
     txt = str(input('Enter your words to encode, Please note to start with a space'))
     secret = ''
@@ -34,7 +63,30 @@ def encode():
         if i in Encode:
             secret += Encode[i]
     print(secret)
-    encode()
+    ask()
 
 
-encode()
+def decode():
+    global txt, secret
+    txt = str(input('Enter your words to decode, Please note to start with a space'))
+    secret = ''
+    for y in txt:
+        if y in Decode:
+            secret += Decode[y]
+    print(secret)
+    ask()
+
+
+def ask():
+    global UserInput
+    UserInput = str(input('decode or encode?'))
+    if UserInput == 'decode':
+        decode()
+    elif UserInput == 'encode':
+        encode()
+    else:
+        print('Invalid input, please try again')
+        ask()
+
+
+ask()
